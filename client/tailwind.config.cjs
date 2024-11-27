f290e400fe575cde8208e57b61e169b71bccdfc9
Martin George <martingeorge191110@gmail.com>
2024-11-27 16:48:20 +0200

    Installing packages && App adjustments

---
+++ client/tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',  // Include index.html
+    './src/**/*.{js,ts,jsx,tsx}',  // Include all React files in src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
