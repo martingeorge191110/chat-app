90ab72515463d8c9b36833747e29ecb19a15340a
Martin George <martingeorge191110@gmail.com>
2024-12-18 18:10:47 +0200

    User router file

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -14,10 +14,16 @@ UserRouter.use(VerifyToken)
 UserRouter.route("/account/").get(user_instance.Verify_account)
 
 
-// UserRouter.route("/profile/")
-//                      .put(
+UserRouter.route("/profile/")
+                     .put(
+                        user_instance.updateInfValid(), user_instance.validation_error,
+                     )
 
-//                      )
 
+UserRouter.route("/search/")
+                     .get(
+                        user_instance.searching_user(), user_instance.validation_error,
+                        user_instance.Search_user
+                     )
 
 export default UserRouter;
