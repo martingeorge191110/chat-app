7db167ae5b23bbe6d68612eb0391c10dbf6eb470
Martin George <martingeorge191110@gmail.com>
2024-12-18 18:11:19 +0200

    User controller, validator

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -33,6 +33,44 @@ class UserController extends UserValidator {
 
       this.response_successfuly(res, 200, "User info retreived Successfuly!", {...user_result})
    }
+
+   public Search_user = async (req: Request, res: Response, next: NextFunction) => {
+      const {category, value} = req.query as {category: "name" | "email", value: string}
+
+      let name: Array<string> = category === "name" ? value.split(" ") : []
+
+      console.log(name)
+      try {
+         const users:  Array<{id: string, first_n: string, last_n: string, avatar: string | null}> = category === "name" ? 
+         await prisma_db.user.findMany({
+            where: {
+               AND: [
+                  {
+                     first_n: {contains: name[0]},
+                     last_n: {contains: name[1] || ""}
+                  }
+               ]
+            }, select: {
+               id: true, first_n: true, last_n: true, avatar: true
+            }
+         }) : 
+         await prisma_db.user.findMany({
+            where: {
+               email: {contains: value}
+            }, select: {
+               id: true, first_n: true, last_n: true, avatar: true
+            }
+         })
+
+         this.response_successfuly(res, 200, "Retreived Successfuly!", users)
+      } catch (err) {
+         return (next(Api_error.create_error("Server error during searching process!", 500)))
+      }
+   }
+
+   public Update_info = async (req: Request, res: Response, next: NextFunction) => {
+      
+   }
 }
 
 export default UserController;
--- server/src/validators/user.validator.ts
+++ server/src/validators/user.validator.ts
@@ -1,4 +1,9 @@
+import { body, Meta, query } from "express-validator";
+import { custom_validator, validator_chain } from "../server.types.ts";
 import GlobalUtilies from "../utilies/global.utilies.ts";
+import prisma_db from "../prisma.db.ts";
+import { User } from "@prisma/client";
+
 
 
 
@@ -6,7 +11,51 @@ import GlobalUtilies from "../utilies/global.utilies.ts";
 class UserValidator extends GlobalUtilies {
 
 
-   
+   public updateInfValid = (): validator_chain => {
+      const feilds = ["first_n", "last_n", "phone", "bio"]
+      return ([
+         ...feilds.map((feild) => {
+            return (
+               body(`${feild}`)
+                  .optional()
+                  .trim().notEmpty().withMessage("This Field must be not empty!")
+                  .custom(async (val: string, {req}: Meta): custom_validator => {
+                     if (feild !== "phone")
+                        return (true)
+
+                     try {
+                        const user: (User | null) = await prisma_db.user.findUnique({
+                           where: {
+                              phone: val
+                           }
+                        })
+
+                        if (!user)
+                           return (true)
+
+                        this.send_mail(user.email, "Some one trying to add your phone number", `
+                              <h1>some one trying to add your phone number as his own number!</h1>
+                           `)
+                        throw (new Error("this is non unique phone number"))
+                     } catch (err) {
+                        throw (err)
+                     }
+                  })
+            )
+         })
+      ])
+   }
+
+
+   public searching_user = (): validator_chain => {
+      return ([
+         query("category")
+            .trim().notEmpty().withMessage("Category is required!")
+            .isIn(["email", "name"]),
+         query("value")
+            .trim().notEmpty().withMessage("Value is required!")
+      ])
+   }
 }
 
 export default UserValidator;
